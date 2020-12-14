2fb741cc5a4cc852bce2c2db528ab8735a959249
billyott <willrichott@gmail.com>
2020-12-14 00:44:42 -0500

    fix day model date validation

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -5,7 +5,7 @@ class DaysController < ApplicationController
         start_date = params[:start_date] ? Date.parse(params[:start_date]) : Date.today - 3
         matched_user = User.all.find{|user| user.id == params[:user_id].to_i}
         if matched_user
-            date_range = start_date .. end_date
+            date_range = (start_date..end_date).to_a
             days = []
             date_range.each do |date|
                 day = Day.all.find{|day| day.user.id == matched_user.id and day.date == date}
