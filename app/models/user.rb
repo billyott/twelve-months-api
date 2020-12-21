39ca7ec159f47800a47fc70588ff8fcd7f81d55d
billyott <willrichott@gmail.com>
2020-12-21 18:28:28 -0500

    update user model to delete dependent children models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-    has_many :days
-    has_many :habits
+    has_many :days, dependent: :delete_all
+    has_many :habits, dependent: :delete_all
     has_many :notes, through: :days
     validates :username, uniqueness: true
     validates :email, uniqueness: true
